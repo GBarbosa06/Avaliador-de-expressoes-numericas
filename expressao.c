41f7902304b957f1eb1f12f09ac31f4455e33ce4
GBarbosa <andradegui2006@gmail.com>
2025-06-01 20:41:26 -0300

    inicio arquivos do trabalho

---
+++ expressao.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "expressao.h"
+
+char *getFormaInFixa(char *Str){
+    
+}
+char *getFormaPosFixa(char *Str){
+    
+}
+float getValorPosFixa(char *StrPosFixa){
+    
+}
+float getValorInFixa(char *StrInFixa){
+    
+}
